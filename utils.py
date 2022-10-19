a5e483f006ca3890c11f070b797cf19179e4ef30
shadowlift <apoorv.pr@gmail.com>
2022-10-19 17:21:36 +0100

    disqus footer

--- utils.py
+++ utils.py
@@ -292,7 +292,7 @@ class Settings:
         "SLUGIFY": "y",
         "HOME_GRAPH": "y",
         "PAGE_GRAPH": "y",
-        "SUBSECTION_SYMBOL": "  ⊂",
+        "SUBSECTION_SYMBOL": "ㅤ⊂ㅤ",
         "LOCAL_GRAPH": "",
         "GRAPH_LINK_REPLACE": "",
         "STRICT_LINE_BREAKS": "y",
