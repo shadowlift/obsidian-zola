b5b887e9eb649d1bfaaea2e1f778b9e690f66104
shadowlift <apoorv.pr@gmail.com>
2022-10-19 15:16:15 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -292,7 +292,7 @@ class Settings:
         "SLUGIFY": "y",
         "HOME_GRAPH": "y",
         "PAGE_GRAPH": "y",
-        "SUBSECTION_SYMBOL": "📁",
+        "SUBSECTION_SYMBOL": "  ⊂",
         "LOCAL_GRAPH": "",
         "GRAPH_LINK_REPLACE": "",
         "STRICT_LINE_BREAKS": "y",
