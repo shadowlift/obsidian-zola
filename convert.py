c2423cf2f3e9f6a6c43b9c98e2fbc30984619738
shadowlift <apoorv.pr@gmail.com>
2023-08-22 17:31:11 +0100

    removing readme

--- convert.py
+++ convert.py
@@ -28,57 +28,61 @@ if __name__ == "__main__":
 
     for path in [raw_dir, *all_paths]:
         doc_path = DocPath(path)
-        if doc_path.is_file:
-            if doc_path.is_md:
-                # Page
-                nodes[doc_path.abs_url] = doc_path.page_title
-                content = doc_path.content
-                parsed_lines: List[str] = []
-                for line in content:
-                    parsed_line, linked = DocLink.parse(line, doc_path)
+        if str(readme) in str(doc_path):
+            pass
+        else:
 
-                    # Fix LaTEX new lines
-                    parsed_line = re.sub(r"\\\\\s*$", r"\\\\\\\\", parsed_line)
+            if doc_path.is_file:
+                if doc_path.is_md:
+                    # Page
+                    nodes[doc_path.abs_url] = doc_path.page_title
+                    content = doc_path.content
+                    parsed_lines: List[str] = []
+                    for line in content:
+                        parsed_line, linked = DocLink.parse(line, doc_path)
 
-                    parsed_lines.append(parsed_line)
+                        # Fix LaTEX new lines
+                        parsed_line = re.sub(r"\\\\\s*$", r"\\\\\\\\", parsed_line)
 
-                    edges.extend([doc_path.edge(rel_path) for rel_path in linked])
+                        parsed_lines.append(parsed_line)
 
+                        edges.extend([doc_path.edge(rel_path) for rel_path in linked])
+
+                    content = [
+                        "---",
+                        f'title: "{doc_path.page_title}"',
+                        f"date: {doc_path.modified}",
+                        f"updated: {doc_path.modified}",
+                        "template: docs/page.html",
+                        "---",
+                        # To add last line-break
+                        "",
+                    ]
+                    doc_path.write(["\n".join(content), *parsed_lines])
+                    print(f"Found page: {doc_path.new_rel_path}")
+                else:
+                    # Resource
+                    doc_path.copy()
+                    print(f"Found resource: {doc_path.new_rel_path}")
+            else:
+                """Section"""
+                # Frontmatter
+                # TODO: sort_by depends on settings
                 content = [
                     "---",
-                    f'title: "{doc_path.page_title}"',
-                    f"date: {doc_path.modified}",
-                    f"updated: {doc_path.modified}",
-                    "template: docs/page.html",
+                    f'title: "{doc_path.section_title}"',
+                    "template: docs/section.html",
+                    f"sort_by: {Settings.options['SORT_BY']}",
+                    f"weight: {section_count}",
+                    "extra:",
+                    f"    sidebar: {doc_path.section_sidebar}",
                     "---",
                     # To add last line-break
                     "",
                 ]
-                doc_path.write(["\n".join(content), *parsed_lines])
-                print(f"Found page: {doc_path.new_rel_path}")
-            else:
-                # Resource
-                doc_path.copy()
-                print(f"Found resource: {doc_path.new_rel_path}")
-        else:
-            """Section"""
-            # Frontmatter
-            # TODO: sort_by depends on settings
-            content = [
-                "---",
-                f'title: "{doc_path.section_title}"',
-                "template: docs/section.html",
-                f"sort_by: {Settings.options['SORT_BY']}",
-                f"weight: {section_count}",
-                "extra:",
-                f"    sidebar: {doc_path.section_sidebar}",
-                "---",
-                # To add last line-break
-                "",
-            ]
-            section_count += 1
-            doc_path.write_to("_index.md", "\n".join(content))
-            print(f"Found section: {doc_path.new_rel_path}")
+                section_count += 1
+                doc_path.write_to("_index.md", "\n".join(content))
+                print(f"Found section: {doc_path.new_rel_path}")
 
     pp(nodes)
     pp(edges)
