9cea464109dab6ab202316b1a3ae533d4d1b17af
shadowlift <apoorv.pr@gmail.com>
2023-08-22 17:31:41 +0100

    Update convert.py

--- convert.py
+++ convert.py
@@ -28,6 +28,7 @@ if __name__ == "__main__":
 
     for path in [raw_dir, *all_paths]:
         doc_path = DocPath(path)
+        print(doc_path)
         if str(readme) in str(doc_path):
             pass
         else:
